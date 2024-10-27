ea9815750947a29e8df3f66c789cbeccb15a5929
khaleel raweh <khaleelvisa@gmail.com>
2024-10-27 16:16:28 +0300

    save all

--- resources/views/partial/backend/navbar.blade.php
+++ resources/views/partial/backend/navbar.blade.php
@@ -94,8 +94,18 @@
                     <div class="p-1">
                         <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
                             <div class="me-3">
-                                <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30"
-                                    alt="userr">
+                                @php
+                                    if (auth()->user()->user_image != null) {
+                                        $user_img = asset('assets/users/' . auth()->user()->user_image);
+
+                                        if (!file_exists(public_path('assets/users/' . auth()->user()->user_image))) {
+                                            $user_img = asset('image/not_found/avator2.webp');
+                                        }
+                                    } else {
+                                        $user_img = asset('image/not_found/avator2.webp');
+                                    }
+                                @endphp
+                                <img class="wd-30 ht-30 rounded-circle" src="{{ $user_img }}" alt="userr">
                             </div>
                             <div class="d-flex justify-content-between flex-grow-1">
                                 <div class="me-4">
@@ -237,17 +247,16 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30" alt="profile">
+                    <img class="wd-30 ht-30 rounded-circle" src="{{ $user_img }}" alt="profile">
                 </a>
                 <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
                     <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
                         <div class="mb-3">
-                            <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80"
-                                alt="">
+                            <img class="wd-80 ht-80 rounded-circle" src="{{ $user_img }}" alt="">
                         </div>
                         <div class="text-center">
-                            <p class="tx-16 fw-bolder">Amiah Burton</p>
-                            <p class="tx-12 text-muted">amiahburton@gmail.com</p>
+                            <p class="tx-16 fw-bolder">{{ auth()->user()->full_name }}</p>
+                            <p class="tx-12 text-muted">{{ auth()->user()->email }}</p>
                         </div>
                     </div>
                     <ul class="list-unstyled p-1">
@@ -270,10 +279,15 @@
                             </a>
                         </li>
                         <li class="dropdown-item py-2">
-                            <a href="javascript:;" class="text-body ms-0">
+                            <a href="javascript:void(0)"
+                                onclick="event.preventDefault();document.getElementById('logout-form').submit();"
+                                class="text-body ms-0">
                                 <i class="me-2 icon-md" data-feather="log-out"></i>
-                                <span>Log Out</span>
+                                <span>{{ __('panel.f_logout') }}</span>
                             </a>
+                            <form action="{{ route('logout') }}" method="POST" id="logout-form" class="d-none">
+                                @csrf
+                            </form>
                         </li>
                     </ul>
                 </div>
