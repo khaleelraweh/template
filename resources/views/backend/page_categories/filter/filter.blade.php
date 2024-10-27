d62d9cfead5b722fd8c588f0bf2948fc67ec063f
khaleel raweh <khaleelvisa@gmail.com>
2024-10-27 18:38:17 +0300

    save all

--- resources/views/backend/page_categories/filter/filter.blade.php
+++ resources/views/backend/page_categories/filter/filter.blade.php
@@ -1,5 +1,5 @@
 <div class="card-body">
-    <form action="{{ route('admin.web_menus.index') }}" method="get">
+    <form action="{{ route('admin.page_categories.index') }}" method="get">
         <div class="row">
             <div class="col-8 col-sm-4 col-md-2">
                 <div class="form-group">
