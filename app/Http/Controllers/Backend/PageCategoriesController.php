ce8a287c14303d3ae97113ad334fa17dc4b97b1c
khaleel raweh <khaleelvisa@gmail.com>
2024-10-27 18:20:13 +0300

    save all

--- app/Http/Controllers/Backend/PageCategoriesController.php
+++ app/Http/Controllers/Backend/PageCategoriesController.php
@@ -3,9 +3,208 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\PageCategory;
 use Illuminate\Http\Request;
 
 class PageCategoriesController extends Controller
 {
-    //
+
+    public function index()
+    {
+        if (!auth()->user()->ability('admin', 'manage_page_categories , show_page_categories')) {
+            return redirect('admin/index');
+        }
+
+        $page_categories = PageCategory::query()
+            ->when(\request()->keyword != null, function ($query) {
+                $query->search(\request()->keyword);
+            })
+            ->when(\request()->status != null, function ($query) {
+                $query->where('status', \request()->status);
+            })
+            ->orderBy(\request()->sort_by ?? 'created_at', \request()->order_by ?? 'desc')
+            ->paginate(\request()->limit_by ?? 100);
+
+        return view('backend.page_categories.index', compact('page_categories'));
+    }
+
+    public function create()
+    {
+        if (!auth()->user()->ability('admin', 'create_page_categories')) {
+            return redirect('admin/index');
+        }
+
+
+        return view('backend.page_categories.create');
+    }
+
+    public function store(PageRequest $request)
+    {
+        if (!auth()->user()->ability('admin', 'create_page_categories')) {
+            return redirect('admin/index');
+        }
+
+
+        $input['title'] = $request->title;
+        $input['content'] = $request->content;
+
+        $input['status']            =   $request->status;
+        $input['created_by'] = auth()->user()->full_name;
+        $published_on = $request->published_on . ' ' . $request->published_on_time;
+        $published_on = new DateTimeImmutable($published_on);
+        $input['published_on'] = $published_on;
+
+        $page = Page::create($input);
+
+        if ($request->hasFile('images') && count($request->images) > 0) {
+
+            $i = $page->photos->count() + 1;
+
+            $images = $request->file('images');
+
+            foreach ($images as $image) {
+                $manager = new ImageManager(new Driver());
+
+                $file_name = $page->slug . '_' . time() . $i . '.' . $image->getClientOriginalExtension();
+                $file_size = $image->getSize();
+                $file_type = $image->getMimeType();
+
+                $img = $manager->read($image);
+                $img->save(base_path('public/assets/pages/' . $file_name));
+
+                $page->photos()->create([
+                    'file_name' => $file_name,
+                    'file_size' => $file_size,
+                    'file_type' => $file_type,
+                    'file_status' => 'true',
+                    'file_sort' => $i,
+                ]);
+                $i++;
+            }
+        }
+
+        if ($page) {
+            return redirect()->route('admin.page_categories.index')->with([
+                'message' => __('panel.created_successfully'),
+                'alert-type' => 'success'
+            ]);
+        }
+
+        return redirect()->route('admin.page_categories.index')->with([
+            'message' => __('panel.something_was_wrong'),
+            'alert-type' => 'danger'
+        ]);
+    }
+
+
+
+    public function show($id)
+    {
+        if (!auth()->user()->ability('admin', 'display_page_categories')) {
+            return redirect('admin/index');
+        }
+        return view('backend.page_categories.show');
+    }
+
+    public function edit($page)
+    {
+        if (!auth()->user()->ability('admin', 'update_page_categories')) {
+            return redirect('admin/index');
+        }
+
+        $page = Page::where('id', $page)->first();
+
+        return view('backend.page_categories.edit', compact('page'));
+    }
+
+    public function update(PageRequest $request, $page)
+    {
+
+        $page = Page::where('id', $page)->first();
+
+        $input['title'] = $request->title;
+        $input['content'] = $request->content;
+
+        $input['status']            =   $request->status;
+        $input['created_by'] = auth()->user()->full_name;
+        $published_on = $request->published_on . ' ' . $request->published_on_time;
+        $published_on = new DateTimeImmutable($published_on);
+        $input['published_on'] = $published_on;
+
+        $page->update($input);
+
+        if ($request->hasFile('images') && count($request->images) > 0) {
+
+            $i = $page->photos->count() + 1;
+
+            $images = $request->file('images');
+
+            foreach ($images as $image) {
+                $manager = new ImageManager(new Driver());
+
+                $file_name = $page->slug . '_' . time() . $i . '.' . $image->getClientOriginalExtension();
+                $file_size = $image->getSize();
+                $file_type = $image->getMimeType();
+
+                $img = $manager->read($image);
+                $img->save(base_path('public/assets/pages/' . $file_name));
+
+                $page->photos()->create([
+                    'file_name' => $file_name,
+                    'file_size' => $file_size,
+                    'file_type' => $file_type,
+                    'file_status' => 'true',
+                    'file_sort' => $i,
+                ]);
+                $i++;
+            }
+        }
+
+        if ($page) {
+            return redirect()->route('admin.page_categories.index')->with([
+                'message' => __('panel.updated_successfully'),
+                'alert-type' => 'success'
+            ]);
+        }
+
+        return redirect()->route('admin.page_categories.index')->with([
+            'message' => __('panel.something_was_wrong'),
+            'alert-type' => 'danger'
+        ]);
+    }
+
+    public function destroy($page)
+    {
+        if (!auth()->user()->ability('admin', 'delete_page_categories')) {
+            return redirect('admin/index');
+        }
+
+        $page = Page::where('id', $page)->first()->delete();
+
+        if ($page) {
+            return redirect()->route('admin.page_categories.index')->with([
+                'message' => __('panel.deleted_successfully'),
+                'alert-type' => 'success'
+            ]);
+        }
+
+        return redirect()->route('admin.page_categories.index')->with([
+            'message' => __('panel.something_was_wrong'),
+            'alert-type' => 'danger'
+        ]);
+    }
+
+    public function remove_image(Request $request)
+    {
+        if (!auth()->user()->ability('admin', 'delete_page_categories')) {
+            return redirect('admin/index');
+        }
+        $page = Page::findOrFail($request->page_id);
+        $image = $page->photos()->where('id', $request->image_id)->first();
+        if (File::exists('assets/pages/' . $image->file_name)) {
+            unlink('assets/pages/' . $image->file_name);
+        }
+        $image->delete();
+        return true;
+    }
 }
