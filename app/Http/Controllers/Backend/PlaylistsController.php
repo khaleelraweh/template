d4d460735cd1253de1d32c181c251f5ae4747fd2
khaleel raweh <khaleelvisa@gmail.com>
2024-10-29 23:03:26 +0300

    save all

--- app/Http/Controllers/Backend/PlaylistsController.php
+++ app/Http/Controllers/Backend/PlaylistsController.php
@@ -177,6 +177,11 @@ class PlaylistsController extends Controller
             }
         }
 
+        $playlist->videoLinks()->delete();
+        if ($request->videoLinks) {
+            $playlist->videoLinks()->createMany($request->videoLinks);
+        }
+
         if ($playlist) {
             return redirect()->route('admin.playlists.index')->with([
                 'message' => __('panel.updated_successfully'),
