3a5d140041dd76e396560f2bca29115e657df5c1
khaleel raweh <khaleelvisa@gmail.com>
2024-10-29 17:00:01 +0300

    save all

--- resources/views/backend/events/create.blade.php
+++ resources/views/backend/events/create.blade.php
@@ -175,7 +175,7 @@
 
                         <div class="row">
                             <div class="col-sm-12 col-md-2 pt-3">
-                                وقت وتاريخ بدء الحدث
+                                {{ __('panel.event_end_time_and_date') }}
                             </div>
                             <div class="col-sm-12 col-md-10 pt-3">
                                 <div class="input-group flatpickr" id="flatpickr-datetime">
