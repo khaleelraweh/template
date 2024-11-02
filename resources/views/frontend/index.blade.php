df929feaed482073e768a764a72c184b32202a5a
khaleel raweh <khaleelvisa@gmail.com>
2024-11-03 00:50:31 +0300

    save all

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -9,19 +9,40 @@
             data-mobile-device-dots="false" data-ipad-device="1" data-ipad-device-nav="false" data-ipad-device-dots="false"
             data-ipad-device2="1" data-ipad-device-nav2="false" data-ipad-device-dots2="false" data-md-device="1"
             data-md-device-nav="true" data-md-device-dots="false">
-            <div class="slider-content slide1">
-                <div class="container">
-                    <div class="content-part">
-                        <div class="sl-sub-title wow bounceInLeft" data-wow-delay="300ms" data-wow-duration="2000ms">
-                            Start to learning today</div>
-                        <h1 class="sl-title wow fadeInRight" data-wow-delay="600ms" data-wow-duration="2000ms">
-                            Online Courses From Leading Experts</h1>
-                        <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
-                            <a class="readon orange-btn main-home" href="#">Find Courses</a>
+
+            @forelse ($main_sliders->where('section' ,1) as $main_slider)
+                @php
+                    if ($main_slider->firstMedia != null && $main_slider->firstMedia->file_name != null) {
+                        $main_slider_img = asset('assets/main_sliders/' . $main_slider->firstMedia->file_name);
+
+                        if (!file_exists(public_path('assets/main_sliders/' . $main_slider->firstMedia->file_name))) {
+                            // $main_slider_img = asset('image/not_found/item_image_not_found.webp');
+                            $main_slider_img = asset('frontend/images/slider/main-home/1.jpg');
+                        }
+                    } else {
+                        // $main_slider_img = asset('image/not_found/item_image_not_found.webp');
+                        $main_slider_img = asset('frontend/images/slider/main-home/1.jpg');
+                    }
+                @endphp
+                <div class="slider-content slide1" style="background-image: url({{ $main_slider_img }})">
+                    <div class="container">
+                        <div class="content-part">
+                            <div class="sl-sub-title wow bounceInLeft" data-wow-delay="300ms" data-wow-duration="2000ms">
+                                {{ $main_slider->subtitle }}
+                            </div>
+                            <h1 class="sl-title wow fadeInRight" data-wow-delay="600ms" data-wow-duration="2000ms">
+                                {{ $main_slider->title }}
+                            </h1>
+                            <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
+                                <a class="readon orange-btn main-home" href="#">Find Courses</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @empty
+            @endforelse
+
+
             <div class="slider-content slide2">
                 <div class="container">
                     <div class="content-part">
