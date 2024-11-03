fab52706fdd0bcc2676b2e430e1324f7a9a94b84
khaleel raweh <khaleelvisa@gmail.com>
2024-11-03 10:06:46 +0300

    save all

--- resources/views/partial/frontend/header.blade.php
+++ resources/views/partial/frontend/header.blade.php
@@ -28,159 +28,36 @@
                                  </div>
                                  <nav class="rs-menu">
                                      <ul class="nav-menu">
-                                         <li class="rs-mega-menu mega-rs menu-item-has-children current-menu-item">
-                                             <a href="index.html">Home</a>
-                                             <ul class="mega-menu">
-                                                 <li class="mega-menu-container">
-                                                     <div class="mega-menu-innner">
-                                                         <div class="single-megamenu">
-                                                             <ul class="sub-menu">
-                                                                 <li class="active"><a href="index.html">Main
-                                                                         Demo</a> </li>
-                                                                 <li><a href="index2.html">Online Course</a>
-                                                                 </li>
-                                                                 <li><a href="index3.html">University 01</a>
-                                                                 </li>
-                                                                 <li><a href="index4.html">E-Learning</a> </li>
-                                                                 <li><a href="index5.html">Distance Learning</a>
-                                                                 </li>
-                                                             </ul>
-                                                         </div>
-                                                         <div class="single-megamenu">
-                                                             <ul class="sub-menu last-sub-menu">
-                                                                 <li><a href="index6.html">Personal Demo</a>
-                                                                 </li>
-                                                                 <li><a href="index7.html">Online Training</a>
-                                                                 </li>
-                                                                 <li><a href="index8.html">Online Learning</a>
-                                                                 </li>
-                                                                 <li><a href="index9.html">Kitchen Coach</a>
-                                                                 </li>
-                                                                 <li><a href="index10.html">University 02</a>
-                                                                 </li>
-                                                             </ul>
-                                                         </div>
-                                                         <div class="single-megamenu">
-                                                             <ul class="sub-menu last-sub-menu">
-                                                                 <li><a href="index11.html">Kindergarten 01</a>
-                                                                 </li>
-                                                                 <li><a href="index12.html">Freelancing
-                                                                         Course</a> </li>
-                                                                 <li><a href="index13.html">Gym Coach</a> </li>
-                                                                 <li><a href="index14.html">Courses Archive</a>
-                                                                 </li>
-                                                                 <li><a href="index15.html">Courses Hub</a> </li>
-                                                             </ul>
-                                                         </div>
-                                                     </div>
-                                                 </li>
-                                             </ul> <!-- //.mega-menu -->
-                                         </li>
-                                         <li class="menu-item-has-children">
-                                             <a href="#">About</a>
-                                             <ul class="sub-menu">
-                                                 <li><a href="about.html">About One</a> </li>
-                                                 <li><a href="about2.html">About Two</a> </li>
-                                             </ul>
-                                         </li>
-
-                                         <li class="menu-item-has-children">
-                                             <a href="#">Courses</a>
-                                             <ul class="sub-menu">
-                                                 <li><a href="course.html">Courses One</a> </li>
-                                                 <li><a href="course2.html">Courses Two</a> </li>
-                                                 <li><a href="course3.html">Courses Three</a> </li>
-                                                 <li><a href="course4.html">Courses Four</a>
-                                                 </li>
-                                                 <li><a href="course5.html">Courses Five</a> </li>
-                                                 <li><a href="course-single.html">Courses Single</a> </li>
-                                             </ul>
-                                         </li>
-
-                                         <li class="menu-item-has-children">
-                                             <a href="#">Pages</a>
-                                             <ul class="sub-menu">
-                                                 <li class="menu-item-has-children right">
-                                                     <a href="#">Team</a>
-                                                     <ul class="sub-menu right">
-                                                         <li><a href="team.html">Team One</a></li>
-                                                         <li><a href="team2.html">Team Two</a></li>
-                                                         <li><a href="team-single.html">Team Single</a></li>
+                                         @foreach ($web_menus->where('section', 1) as $menu)
+                                             <li
+                                                 class=" {{ count($menu->appearedChildren) > 0 ? 'menu-item-has-children has-children' : '' }}">
+                                                 <a href="{{ $menu->link }}">{{ $menu->title }}</a>
+                                                 @if (count($menu->appearedChildren) > 0)
+                                                     <ul class="sub-menu">
+                                                         @foreach ($menu->appearedChildren as $sub_menu)
+                                                             <li
+                                                                 class="menu-item-has-children {{ count($sub_menu->appearedChildren) > 0 ? 'has-children' : '' }}">
+                                                                 <a
+                                                                     href="{{ $sub_menu->link }}">{{ $sub_menu->title }}</a>
+                                                                 @if (count($sub_menu->appearedChildren) > 0)
+                                                                     <ul class="sub-menu">
+                                                                         @foreach ($sub_menu->appearedChildren as $sub_sub_menu)
+                                                                             <li>
+                                                                                 <a
+                                                                                     href="{{ $sub_sub_menu->link }}">{{ $sub_sub_menu->title }}</a>
+                                                                             </li>
+                                                                         @endforeach
+                                                                     </ul>
+                                                                 @endif
+                                                             </li>
+                                                         @endforeach
                                                      </ul>
-                                                 </li>
-                                                 <li class="menu-item-has-children">
-                                                     <a href="#">Event</a>
-                                                     <ul class="sub-menu right">
-                                                         <li><a href="events-style1.html">Event One</a></li>
-                                                         <li><a href="events-style2.html">Event Two</a></li>
-                                                         <li><a href="events-style3.html">Event Three</a></li>
-                                                     </ul>
-                                                 </li>
-                                                 <li class="menu-item-has-children">
-                                                     <a href="#">Gallery</a>
-                                                     <ul class="sub-menu right">
-                                                         <li><a href="gallery-style1.html">Gallery One</a></li>
-                                                         <li><a href="gallery-style2.html">Gallery Two</a></li>
-                                                         <li><a href="gallery-style3.html">Gallery Three</a></li>
-                                                     </ul>
-                                                 </li>
-                                                 <li class="menu-item-has-children">
-                                                     <a href="#">Shop</a>
-                                                     <ul class="sub-menu right">
-                                                         <li><a href="shop.html">Shop</a></li>
-                                                         <li><a href="shop-single.html">Shop Single</a></li>
-                                                         <li><a href="cart.html">Cart</a></li>
-                                                         <li><a href="checkout.html">Checkout</a></li>
-                                                     </ul>
-                                                 </li>
-                                                 <li class="menu-item-has-children">
-                                                     <a href="#">Others</a>
-                                                     <ul class="sub-menu right">
-                                                         <li><a href="faq.html">FAQ</a></li>
-                                                         <li><a href="error.html">404 Page</a></li>
-                                                         <li><a href="login.html">Login</a></li>
-                                                         <li><a href="register.html">Register</a></li>
-                                                     </ul>
-                                                 </li>
-                                             </ul>
-                                         </li>
-
-                                         <li class="menu-item-has-children">
-                                             <a href="#">Blog</a>
-                                             <ul class="sub-menu">
-                                                 <li><a href="blog.html">Blog</a></li>
-                                                 <li class="menu-item-has-children">
-                                                     <a href="#">Blog Sidebar</a>
-                                                     <ul class="sub-menu right">
-                                                         <li><a href="blog-left.html">Blog Left Sidebar</a></li>
-                                                         <li><a href="blog-right.html">Blog Right Sidebar</a>
-                                                         </li>
-                                                     </ul>
-                                                 </li>
-                                                 <li class="menu-item-has-children">
-                                                     <a href="#">Single Post</a>
-                                                     <ul class="sub-menu right">
-                                                         <li><a href="blog-post-left.html">Post Left Sidebar</a>
-                                                         </li>
-                                                         <li><a href="blog-post-right.html">Post Right
-                                                                 Sidebar</a></li>
-                                                         <li><a href="blog-single.html">Full Width Post</a></li>
-                                                     </ul>
-                                                 </li>
-                                             </ul>
-                                         </li>
-
-                                         <li class="menu-item-has-children">
-                                             <a href="#">Contact</a>
-                                             <ul class="sub-menu">
-                                                 <li><a href="contact.html">Contact One</a> </li>
-                                                 <li><a href="contact2.html">Contact Two</a> </li>
-                                                 <li><a href="contact3.html">Contact Three</a> </li>
-                                                 <li><a href="contact4.html">Contact Four</a> </li>
-                                             </ul>
-                                         </li>
+                                                 @endif
+                                             </li>
+                                         @endforeach
                                      </ul> <!-- //.nav-menu -->
                                  </nav>
+
                              </div> <!-- //.main-menu -->
 
                          </div>
