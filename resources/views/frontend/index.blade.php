053681d64c5c389081d9f48c9a7377d19bbdc31a
khaleel raweh <khaleelvisa@gmail.com>
2024-11-03 21:00:25 +0300

    save all

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -50,21 +50,6 @@
                 </div>
             @empty
             @endforelse
-
-
-            {{-- <div class="slider-content slide2">
-                <div class="container">
-                    <div class="content-part">
-                        <div class="sl-sub-title wow bounceInLeft" data-wow-delay="300ms" data-wow-duration="2000ms">
-                            Start to learning today</div>
-                        <h1 class="sl-title wow fadeInRight" data-wow-delay="600ms" data-wow-duration="2000ms">
-                            Explore Interests and Career With Courses</h1>
-                        <div class="sl-btn wow fadeInUp" data-wow-delay="900ms" data-wow-duration="2000ms">
-                            <a class="readon orange-btn main-home" href="#">Find Courses</a>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
         </div>
 
         <!-- Features Section start -->
@@ -92,43 +77,197 @@
                             </div>
                         </div>
                     @endforeach
-
-                    {{-- <div class="col-lg-4 col-md-12 md-mb-30">
-                        <div class="features-wrap">
-                            <div class="icon-part">
-                                <img src="{{ asset('frontend/images/features/icon/2.png') }}" alt="">
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title">
-                                    <span class="watermark">Expert instruction</span>
-                                </h4>
-                                <p class="dese">
-                                    Find the right instructor
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-12">
-                        <div class="features-wrap">
-                            <div class="icon-part">
-                                <img src="{{ asset('frontend/images/features/icon/1.png') }}" alt="">
-                            </div>
-                            <div class="content-part">
-                                <h4 class="title">
-                                    <span class="watermark">Lifetime access</span>
-                                </h4>
-                                <p class="dese">
-                                    Learn on your schedule
-                                </p>
-                            </div>
-                        </div>
-                    </div> --}}
                 </div>
             </div>
         </div>
         <!-- Features Section End -->
     </div>
     <!-- Slider Section End -->
+
+    <!-- Blog Section Start -->
+    {{-- <div id="rs-blog" class="rs-blog main-home pb-100 pt-100 md-pt-70 md-pb-70"> --}}
+    <div class="row">
+        <div class="col-sm-12 col-md-5"></div>
+        <div class="col-sm-12 col-md-7">
+            <div id="rs-blog" class="rs-blog main-home pb-100 pt-100 md-pt-70 md-pb-70">
+                <div class="container">
+                    <div class="sec-title3 text-center mb-50">
+                        <div class="sub-title"> News Update</div>
+                        <h2 class="title"> Latest News & events</h2>
+                    </div>
+                    <div class="rs-carousel owl-carousel" data-loop="true" data-items="2" data-margin="30"
+                        data-autoplay="true" data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800"
+                        data-dots="false" data-nav="false" data-nav-speed="false" data-center-mode="false"
+                        data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="false"
+                        data-ipad-device="2" data-ipad-device-nav="false" data-ipad-device-dots="false"
+                        data-ipad-device2="1" data-ipad-device-nav2="false" data-ipad-device-dots2="false"
+                        data-md-device="2" data-md-device-nav="false" data-md-device-dots="false">
+                        <div class="blog-item">
+                            <div class="image-part">
+                                <img src="{{ asset('frontend/images/blog/style2/1.jpg') }}" alt="">
+                            </div>
+                            <div class="blog-content">
+                                <ul class="blog-meta">
+                                    <li><i class="fa fa-user-o"></i> Admin</li>
+                                    <li><i class="fa fa-calendar"></i>December 15, 2020</li>
+                                </ul>
+                                <h3 class="title"><a href="blog-single.html">Education is The Process of Facilitating
+                                        Learning</a></h3>
+                                <div class="desc">the acquisition of knowledge, skills, values befs, and habits.
+                                    Educational
+                                    methods include teach ing, training, storytelling</div>
+                                <div class="btn-btm">
+                                    <div class="cat-list">
+                                        <ul class="post-categories">
+                                            <li><a href="#">College</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="rs-view-btn">
+                                        <a href="#">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-item">
+                            <div class="image-part">
+                                <img src="{{ asset('frontend/images/blog/style2/2.jpg') }}" alt="">
+                            </div>
+                            <div class="blog-content">
+                                <ul class="blog-meta">
+                                    <li><i class="fa fa-user-o"></i> Admin</li>
+                                    <li><i class="fa fa-calendar"></i>October 15, 2020</li>
+                                </ul>
+                                <h3 class="title"><a href="blog-single.html">High school program starting soon 2021 </a>
+                                </h3>
+                                <div class="desc">the acquisition of knowledge, skills, values befs, and habits.
+                                    Educational
+                                    methods include teach ing, training, storytelling</div>
+                                <div class="btn-btm">
+                                    <div class="cat-list">
+                                        <ul class="post-categories">
+                                            <li><a href="#">College</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="rs-view-btn">
+                                        <a href="#">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-item">
+                            <div class="image-part">
+                                <img src="{{ asset('frontend/images/blog/style2/3.jpg') }}" alt="">
+                            </div>
+                            <div class="blog-content">
+                                <ul class="blog-meta">
+                                    <li><i class="fa fa-user-o"></i> Admin</li>
+                                    <li><i class="fa fa-calendar"></i>April 25, 2020</li>
+                                </ul>
+                                <h3 class="title"><a href="blog-single.html">Shutdown of schools extended to Aug 31 </a>
+                                </h3>
+                                <div class="desc">the acquisition of knowledge, skills, values befs, and habits.
+                                    Educational
+                                    methods include teach ing, training, storytelling</div>
+                                <div class="btn-btm">
+                                    <div class="cat-list">
+                                        <ul class="post-categories">
+                                            <li><a href="#">College</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="rs-view-btn">
+                                        <a href="#">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-item">
+                            <div class="image-part">
+                                <img src="{{ asset('frontend/images/blog/style2/4.jpg') }}" alt="">
+                            </div>
+                            <div class="blog-content">
+                                <ul class="blog-meta">
+                                    <li><i class="fa fa-user-o"></i> Admin</li>
+                                    <li><i class="fa fa-calendar"></i>June 20, 2010</li>
+                                </ul>
+                                <h3 class="title"><a href="blog-single.html">This is a great source of content for
+                                        anyone…
+                                    </a></h3>
+                                <div class="desc">the acquisition of knowledge, skills, values befs, and habits.
+                                    Educational
+                                    methods include teach ing, training, storytelling</div>
+                                <div class="btn-btm">
+                                    <div class="cat-list">
+                                        <ul class="post-categories">
+                                            <li><a href="#">College</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="rs-view-btn">
+                                        <a href="#">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-item">
+                            <div class="image-part">
+                                <img src="{{ asset('frontend/images/blog/style2/5.jpg') }}" alt="">
+                            </div>
+                            <div class="blog-content">
+                                <ul class="blog-meta">
+                                    <li><i class="fa fa-user-o"></i> Admin</li>
+                                    <li><i class="fa fa-calendar"></i>August 30, 2020</li>
+                                </ul>
+                                <h3 class="title"><a href="blog-single.html">Pandemic drives millions from Latin
+                                        America’s</a></h3>
+                                <div class="desc">the acquisition of knowledge, skills, values befs, and habits.
+                                    Educational
+                                    methods include teach ing, training, storytelling</div>
+                                <div class="btn-btm">
+                                    <div class="cat-list">
+                                        <ul class="post-categories">
+                                            <li><a href="#">College</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="rs-view-btn">
+                                        <a href="#">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-item">
+                            <div class="image-part">
+                                <img src="{{ asset('frontend/images/blog/style2/6.jpg') }}" alt="">
+                            </div>
+                            <div class="blog-content">
+                                <ul class="blog-meta">
+                                    <li><i class="fa fa-user-o"></i> Admin</li>
+                                    <li><i class="fa fa-calendar"></i>May 10, 2020</li>
+                                </ul>
+                                <h3 class="title"><a href="blog-single.html">Modern School the lovely valley team
+                                        work</a>
+                                </h3>
+                                <div class="desc">the acquisition of knowledge, skills, values befs, and habits.
+                                    Educational
+                                    methods include teach ing, training, storytelling</div>
+                                <div class="btn-btm">
+                                    <div class="cat-list">
+                                        <ul class="post-categories">
+                                            <li><a href="#">College</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="rs-view-btn">
+                                        <a href="#">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- </div> --}}
+    <!-- Blog Section End -->
 
     <!-- Categories Section Start -->
     <div id="rs-categories" class="rs-categories main-home pt-90 pb-100 md-pt-60 md-pb-40">
