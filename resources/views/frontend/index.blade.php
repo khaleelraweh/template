5e1bc5c9166e923d9ef07d5f8f6eb0889188399a
khaleel raweh <khaleelvisa@gmail.com>
2024-11-04 18:34:17 +0300

    save all

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -342,8 +342,6 @@
 
     <!-- start statistics -->
     <div class=" main-home event-bg pt-100 pb-100 md-pt-70 md-pb-70">
-
-
         <div class="rs-counter style2-about">
             <div class="container">
                 <div class="row couter-area">
@@ -357,30 +355,106 @@
                         </div>
                     @endforeach
 
-
-                    {{-- <div class="col-lg-3 col-md-6 md-mb-30 ">
-                        <div class="counter-item text-center">
-                            <h2 class="rs-count plus">4523</h2>
-                            <h4 class="title mb-0">Enrolled Learners</h4>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 sm-mb-30">
-                        <div class="counter-item text-center">
-                            <h2 class="rs-count plus">120</h2>
-                            <h4 class="title mb-0">Online Instructors</h4>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="counter-item text-center">
-                            <h2 class="rs-count percent">100</h2>
-                            <h4 class="title mb-0">Satisfaction Rate</h4>
-                        </div>
-                    </div> --}}
                 </div>
             </div>
         </div>
     </div>
     <!-- end statistics -->
+
+    <!-- Degree Section Start -->
+    <div class="rs-degree style1 modify gray-bg pt-100 pb-70 md-pt-70 md-pb-40">
+        <div class="container">
+            <div class="row y-middle">
+                <div class="col-lg-4 col-md-6 mb-30">
+                    <div class="sec-title wow fadeInUp" data-wow-delay="300ms" data-wow-duration="2000ms">
+                        <div class="sub-title primary">Degree categoris</div>
+                        <h2 class="title mb-0">Successfully Complete A Degree at Educavo University</h2>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-30">
+                    <div class="degree-wrap">
+                        <img src="{{ asset('frontend/images/degrees/1.jpg') }}" alt="">
+                        <div class="title-part">
+                            <h4 class="title">Undergraduate</h4>
+                        </div>
+                        <div class="content-part">
+                            <h4 class="title"><a href="#">Undergraduate</a></h4>
+                            <p class="desc">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+                                quo minus id quod </p>
+                            <div class="btn-part">
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-30">
+                    <div class="degree-wrap">
+                        <img src="{{ asset('frontend/images/degrees/2.jpg') }}" alt="">
+                        <div class="title-part">
+                            <h4 class="title">Postgraduate</h4>
+                        </div>
+                        <div class="content-part">
+                            <h4 class="title"><a href="#">Postgraduate</a></h4>
+                            <p class="desc">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+                                quo minus id quod </p>
+                            <div class="btn-part">
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-30">
+                    <div class="degree-wrap">
+                        <img src="{{ asset('frontend/images/degrees/3.jpg') }}" alt="">
+                        <div class="title-part">
+                            <h4 class="title">PHD Scholarships</h4>
+                        </div>
+                        <div class="content-part">
+                            <h4 class="title"><a href="#">PHD Scholarships</a></h4>
+                            <p class="desc">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+                                quo minus id quod </p>
+                            <div class="btn-part">
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-30">
+                    <div class="degree-wrap">
+                        <img src="{{ asset('frontend/images/degrees/4.jpg') }}" alt="">
+                        <div class="title-part">
+                            <h4 class="title">International Hubs</h4>
+                        </div>
+                        <div class="content-part">
+                            <h4 class="title"><a href="#">International Hubs</a></h4>
+                            <p class="desc">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+                                quo minus id quod </p>
+                            <div class="btn-part">
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-30">
+                    <div class="degree-wrap">
+                        <img src="{{ asset('frontend/images/degrees/5.jpg') }}" alt="">
+                        <div class="title-part">
+                            <h4 class="title">PHD Scholarships</h4>
+                        </div>
+                        <div class="content-part">
+                            <h4 class="title"><a href="#">PHD Scholarships</a></h4>
+                            <p class="desc">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+                                quo minus id quod </p>
+                            <div class="btn-part">
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Degree Section End -->
 
     <!-- Categories Section Start -->
     <div id="rs-categories" class="rs-categories main-home pt-90 pb-100 md-pt-60 md-pb-40">
