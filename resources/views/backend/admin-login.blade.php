9fb1800413edd693fc9b572a887d5a54d64cc7d9
khaleel raweh <khaleelvisa@gmail.com>
2024-11-10 16:55:35 +0300

    save all

--- resources/views/backend/admin-login.blade.php
+++ resources/views/backend/admin-login.blade.php
@@ -20,18 +20,29 @@
                                         <h5 class="text-muted fw-normal mb-4">
                                             {{ __('panel.welcome_back_login_to_your_account') }}</h5>
                                         <form class="forms-sample" action="{{ route('login') }}" method="POST">
+                                            @csrf
                                             <div class="mb-3">
                                                 <label for="userEmail" class="form-label">{{ __('panel.user_name') }}
                                                     {{ __('panel.or') }} {{ __('panel.f_email') }}</label>
-                                                <input type="email" class="form-control" id="userEmail"
+
+                                                <input class="form-control" type="text" name='username' id="userEmail"
+                                                    value="{{ old('username') }}"
                                                     placeholder="{{ __('panel.f_user_email') }}">
+                                                @error('username')
+                                                    <span class="text-danger">{{ $message }}</span>
+                                                @enderror
+
                                             </div>
                                             <div class="mb-3">
                                                 <label for="userPassword"
                                                     class="form-label">{{ __('panel.f_password') }}</label>
                                                 <input type="password" class="form-control" id="userPassword"
-                                                    autocomplete="current-password"
+                                                    name="password" autocomplete="current-password"
                                                     placeholder="{{ __('panel.f_password') }}">
+                                                @error('userPassword')
+                                                    <span class="text-danger">{{ $message }}</span>
+                                                @enderror
+
                                             </div>
                                             <div class="form-check mb-3">
                                                 <input type="checkbox" class="form-check-input" id="authCheck">
@@ -40,11 +51,11 @@
                                                 </label>
                                             </div>
                                             <div>
-                                                <a href="../../dashboard.html"
-                                                    class="btn w-100 btn-primary me-2 mb-2 mb-md-0 text-white">{{ __('panel.f_login') }}</a>
-
+                                                <button class="btn w-100 btn-primary me-2 mb-2 mb-md-0 text-white"
+                                                    type="submit">{{ __('panel.f_login') }}</button>
                                             </div>
-                                            <a href="register.html" class="d-block mt-3 text-muted">Not a user? Sign up</a>
+                                            <a href="{{ route('admin.recover-password') }}"
+                                                class="d-block mt-3 text-muted">{{ __('panel.f_forgot_password') }}</a>
                                         </form>
                                     </div>
                                 </div>
@@ -55,75 +66,5 @@
 
             </div>
         </div>
-    </div>
-
-    <div class="card">
-        <div class="card-body">
-
-            <div class="text-center mt-4">
-                <div class="mb-3">
-                    <a href="{{ route('admin.index') }}" class="auth-logo">
-                        <img src="{{ asset('backend/images/logo-dark.png') }}" height="30" class="logo-dark mx-auto"
-                            alt="">
-                        <img src="{{ asset('backend/images/logo-light.png') }}" height="30" class="logo-light mx-auto"
-                            alt="">
-                    </a>
-                </div>
-            </div>
-
-            <h4 class="text-muted text-center font-size-18"><b>Sign In</b></h4>
-
-            <div class="p-3">
-                <form class="form-horizontal mt-3" action="{{ route('login') }}" method="POST" class="user">
-                    @csrf
-                    <div class="form-group mb-3 row">
-                        <div class="col-12">
-                            <input class="form-control" type="text" name='username' id="username"
-                                value="{{ old('username') }}" placeholder="Username">
-                            @error('username')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group mb-3 row">
-                        <div class="col-12">
-                            <input class="form-control" type="password" name="password" id="password"
-                                placeholder="Password">
-                            @error('password')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group mb-3 row">
-                        <div class="col-12">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="remember" name="remember"
-                                    {{ old('remember') ? 'checked' : '' }}>
-                                <label class="form-label ms-1" for="remember">Remember me</label>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="form-group mb-3 text-center row mt-3 pt-1">
-                        <div class="col-12">
-                            <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Log In</button>
-                        </div>
-                    </div>
-
-
-                </form>
-                <div class="form-group mb-0 row mt-2">
-                    <div class="col-sm-7 mt-3">
-                        <a href="{{ route('admin.recover-password') }}" class="text-muted"><i class="mdi mdi-lock"></i>
-                            Forgot your password?</a>
-                    </div>
-                </div>
-            </div>
-            <!-- end -->
-        </div>
-        <!-- end cardbody -->
     </div>
 @endsection
