1d58cd6d710037c02ce0c08573c8163571ac5644
khaleel raweh <khaleelvisa@gmail.com>
2024-11-25 21:47:04 +0300

    save all

---
+++ resources/views/frontend/home/president_speech.blade.php
@@ -0,0 +1,22 @@
+<div class="rs-cta main-home d-none">
+    <div class="partition-bg-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="offset-lg-6"></div>
+                <div class="col-lg-6 pl-70 md-pl-15">
+                    <div class="sec-title3 mb-40">
+                        <h2 class="title white-color mb-16">20% Offer Running - Join Today</h2>
+                        <div class="desc white-color pr-100 md-pr-0">We denounce with righteous indignation and
+                            dislike men who are so beguiled and demoralized by the charms of pleasure of your
+                            moment, so blinded by desire those who fail in their duty through weakness. These
+                            cases are perfectly simple and easy every pleasure is to be welcomed and every pain
+                            avoided.</div>
+                    </div>
+                    <div class="btn-part">
+                        <a class="readon orange-btn transparent" href="#">Register Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
