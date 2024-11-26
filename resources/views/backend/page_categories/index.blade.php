80be7d89d34d57f93defac1681f712dd231d041e
khaleel raweh <khaleelvisa@gmail.com>
2024-11-26 16:34:31 +0300

    save all

--- resources/views/backend/page_categories/index.blade.php
+++ resources/views/backend/page_categories/index.blade.php
@@ -78,22 +78,41 @@
                             </td>
                             <td>
                                 <div class="btn-group btn-group-sm">
-                                    <a href="{{ route('admin.page_categories.edit', $page_category->id) }}"
-                                        class="btn btn-primary" title="Edit the page_category">
-                                        <i class="fa fa-edit"></i>
-                                    </a>
-                                    <a href="javascript:void(0);" class="btn btn-success copyButton"
-                                        data-copy-text="https://ibbuniv.era-t.com/page_categories/{{ $page_category->slug }}"
-                                        title="Copy the link">
-                                        <i class="far fa-copy"></i>
-                                    </a>
-                                    <span class="copyMessage" style="display:none;">{{ __('panel.copied') }}</span>
+
 
                                     @if ($page_category->pages->count() > 0)
-                                        <a href="javascript:void(0);" onclick="showPageCagegoryAlert()"
-                                            class="btn btn-danger">
-                                            <i class="fa fa-trash"></i>
-                                        </a>
+                                        <div class="dropdown mb-2 ">
+                                            <a type="button" class="d-flex" id="dropdownMenuButton"
+                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="icon-lg text-muted pb-3px" data-feather="more-vertical"></i>
+                                                خيارات
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
+                                                    viewBox="0 0 25 15" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-chevron-down link-arrow">
+                                                    <polyline points="6 9 12 15 18 9"></polyline>
+                                                </svg>
+                                            </a>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item d-flex align-items-center"
+                                                    href="{{ route('admin.page_categories.edit', $page_category->id) }}"><i
+                                                        data-feather="edit-2" class="icon-sm me-2"></i> <span
+                                                        class="">Edit</span></a>
+                                                <a class="dropdown-item d-flex align-items-center" href="javascript:;"
+                                                    onclick="showPageCagegoryAlert()"><i data-feather="trash"
+                                                        class="icon-sm me-2"></i> <span class="">Delete</span></a>
+
+                                                <a href="javascript:void(0);"
+                                                    class="dropdown-item d-flex align-items-center btn btn-success copyButton"
+                                                    data-copy-text="https://ibbuniv.era-t.com/page_categories/{{ $page_category->slug }}"
+                                                    title="Copy the link">
+                                                    <i data-feather="copy" class="icon-sm me-2"></i>
+                                                    <span class="">Copy</span>
+                                                </a>
+                                                <span class="copyMessage"
+                                                    style="display:none;">{{ __('panel.copied') }}</span>
+                                            </div>
+                                        </div>
                                     @else
                                         <a href="javascript:void(0);" onclick="confirmDelete({{ $page_category->id }})"
                                             class="btn btn-danger">
