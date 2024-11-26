79b24640f262a43dc7f8f60f9ab201eabe19b093
khaleel raweh <khaleelvisa@gmail.com>
2024-11-26 23:53:14 +0300

    save all

--- public/backend/js/flatpickr.js
+++ public/backend/js/flatpickr.js
@@ -1,38 +1,37 @@
-// npm package: flatpickr
-// github link: https://github.com/flatpickr/flatpickr
-
-$(function() {
+$(function () {
   'use strict';
 
   const locale = "{{ app()->getLocale() }}";
 
   // date picker
   if ($('#flatpickr-date').length) {
-    flatpickr("#flatpickr-date", {
-      wrap: true,
-      dateFormat: "Y-m-d",
-      minDate: "today", // Prevent dates before today
-    });
+      flatpickr("#flatpickr-date", {
+          wrap: true,
+          dateFormat: "Y-m-d",
+          minDate: "today", // Prevent dates before today
+      });
   }
 
   // time picker
   if ($('#flatpickr-time').length) {
-    flatpickr("#flatpickr-time", {
-      wrap: true,
-      enableTime: true,
-      noCalendar: true,
-      dateFormat: "H:i",
-    });
+      flatpickr("#flatpickr-time", {
+          wrap: true,
+          enableTime: true,
+          noCalendar: true,
+          dateFormat: "H:i",
+          defaultDate: new Date(), // Set default time to now
+      });
   }
 
   // datetime picker
   if ($('#flatpickr-datetime').length) {
-    flatpickr("#flatpickr-datetime", {
-      enableTime: true,
-      wrap: true,
-      dateFormat: "Y/m/d h:i K",
-      minDate: "today", // Prevent dates before today
-      locale: typeof flatPickrLanguage !== 'undefined' ? flatPickrLanguage : 'en',
-    });
+      flatpickr("#flatpickr-datetime", {
+          enableTime: true,
+          wrap: true,
+          dateFormat: "Y/m/d h:i K",
+          minDate: "today", // Prevent dates before today
+          locale: typeof flatPickrLanguage !== 'undefined' ? flatPickrLanguage : 'en',
+          defaultDate: new Date(), // Set default date and time to now
+      });
   }
 });
