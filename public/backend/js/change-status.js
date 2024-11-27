720b7f347cbced269553bfbbf58b53252cae2dc1
khaleel raweh <khaleelvisa@gmail.com>
2024-11-27 23:31:54 +0300

    save all

--- public/backend/js/change-status.js
+++ public/backend/js/change-status.js
@@ -134,9 +134,9 @@ $(document).ready(function(){
             data:{status:status,new_id:new_id},
             success:function(resp){
                 if(resp['status']==0){
-                    $("#post-"+new_id).html("<i class='fas fa-toggle-off fa-lg text-warning' aria-hidden='true' status='Inactive' style='font-size:1.6em' />");
+                    $("#new-"+new_id).html("<i class='fas fa-toggle-off fa-lg text-warning' aria-hidden='true' status='Inactive' style='font-size:1.6em' />");
                 }else if (resp['status'] ==1 ){
-                    $("#post-"+new_id).html("<i class='fas fa-toggle-on fa-lg text-success' aria-hidden='true' status='Active' style='font-size:1.6em' />");
+                    $("#new-"+new_id).html("<i class='fas fa-toggle-on fa-lg text-success' aria-hidden='true' status='Active' style='font-size:1.6em' />");
                 }
             },error:function(){
                 alert("Error");
