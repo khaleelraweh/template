e6a07101132c6f0c3793f8c1359076c16e23e5df
khaleel raweh <khaleelvisa@gmail.com>
2024-11-30 22:55:07 +0300

    save all

--- public/backend/js/change-status.js
+++ public/backend/js/change-status.js
@@ -202,13 +202,13 @@ $(document).ready(function(){
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
             type:'post',
-            url:'/admin/events/update-event-status',
+            url:'/admin/albums/update-album-status',
             data:{status:status,album_id:album_id},
             success:function(resp){
                 if(resp['status']==0){
-                    $("#event-"+album_id).html("<i class='fas fa-toggle-off fa-lg text-warning' aria-hidden='true' status='Inactive' style='font-size:1.6em' />");
+                    $("#album-"+album_id).html("<i class='fas fa-toggle-off fa-lg text-warning' aria-hidden='true' status='Inactive' style='font-size:1.6em' />");
                 }else if (resp['status'] ==1 ){
-                    $("#event-"+album_id).html("<i class='fas fa-toggle-on fa-lg text-success' aria-hidden='true' status='Active' style='font-size:1.6em' />");
+                    $("#album-"+album_id).html("<i class='fas fa-toggle-on fa-lg text-success' aria-hidden='true' status='Active' style='font-size:1.6em' />");
                 }
             },error:function(){
                 alert("Error");
