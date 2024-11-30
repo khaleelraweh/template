0d1669a3e159f5a2e517da358485ad90f49ddbc1
khaleel raweh <khaleelvisa@gmail.com>
2024-11-30 23:28:35 +0300

    save all

--- resources/views/backend/playlists/index.blade.php
+++ resources/views/backend/playlists/index.blade.php
@@ -50,7 +50,7 @@
                         <th class="wd-40p border-bottom-0">{{ __('panel.title') }}</th>
                         <th class="wd-15p border-bottom-0 d-none d-sm-table-cell ">{{ __('panel.author') }}</th>
                         <th class="wd-15p border-bottom-0 d-none d-sm-table-cell ">{{ __('panel.status') }}</th>
-                        <th class="wd-15p border-bottom-0 d-none d-sm-table-cell ">{{ __('panel.created_at') }}</th>
+                        <th class="wd-15p border-bottom-0 d-none d-sm-table-cell ">{{ __('panel.published_on') }}</th>
                         <th class="text-center border-bottom-0" style="width:30px;">{{ __('panel.actions') }}</th>
                     </tr>
                 </thead>
