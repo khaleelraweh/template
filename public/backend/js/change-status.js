a19a23be9d1719fd95e9e746c83079a33e379572
khaleel raweh <khaleelvisa@gmail.com>
2024-11-30 23:22:48 +0300

    save all

--- public/backend/js/change-status.js
+++ public/backend/js/change-status.js
@@ -216,5 +216,29 @@ $(document).ready(function(){
         });
     });
 
+    //  updatePlaylistStatus Status
+    $(document).on("click",".updatePlaylistStatus",function(){
+        var status = $(this).children("i").attr("status");
+        var playlist_id = $(this).attr("playlist_id");
+
+        $.ajax({
+            headers:{
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            type:'post',
+            url:'/admin/playlists/update-playlist-status',
+            data:{status:status,playlist_id:playlist_id},
+            success:function(resp){
+                if(resp['status']==0){
+                    $("#playlist-"+playlist_id).html("<i class='fas fa-toggle-off fa-lg text-warning' aria-hidden='true' status='Inactive' style='font-size:1.6em' />");
+                }else if (resp['status'] ==1 ){
+                    $("#playlist-"+playlist_id).html("<i class='fas fa-toggle-on fa-lg text-success' aria-hidden='true' status='Active' style='font-size:1.6em' />");
+                }
+            },error:function(){
+                alert("Error");
+            }
+        });
+    });
+
 
 });
