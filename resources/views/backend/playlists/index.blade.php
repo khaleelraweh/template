d062ede45e05b21f8c2bf136ecddbf8a8be6714c
khaleel raweh <khaleelvisa@gmail.com>
2024-11-30 23:15:09 +0300

    save all

--- resources/views/backend/playlists/index.blade.php
+++ resources/views/backend/playlists/index.blade.php
@@ -10,14 +10,12 @@
                     <i class="fa fa-folder"></i>
                     {{ __('panel.manage_playlists') }}
                 </h3>
-                <ul class="breadcrumb pt-2">
+                <ul class="breadcrumb pt-3">
                     <li>
                         <a href="{{ route('admin.index') }}">{{ __('panel.main') }}</a>
                         @if (config('locales.languages')[app()->getLocale()]['rtl_support'] == 'rtl')
-                            {{-- <i class="fa fa-solid fa-chevron-left chevron"></i> --}}
                             /
                         @else
-                            {{-- <i class="fa fa-solid fa-chevron-right chevron"></i> --}}
                             \
                         @endif
                     </li>
@@ -78,7 +76,7 @@
                                 {{ $playlist->created_at->format('Y/m/d') }}
                             </td>
                             <td>
-                                <div class="btn-group btn-group-sm">
+                                {{-- <div class="btn-group btn-group-sm">
                                     <a href="{{ route('admin.playlists.edit', $playlist->id) }}" class="btn btn-primary"
                                         title="Edit the playlist">
                                         <i class="fa fa-edit"></i>
@@ -101,7 +99,53 @@
                                     class="d-none" id="delete-product-category-{{ $playlist->id }}">
                                     @csrf
                                     @method('DELETE')
-                                </form>
+                                </form> --}}
+                                <div class="btn-group btn-group-sm">
+                                    <div class="dropdown mb-2 ">
+                                        <a type="button" class="d-flex" id="dropdownMenuButton" data-bs-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
+                                            <i class="icon-lg text-muted pb-3px" data-feather="more-vertical"></i>
+                                            {{ __('panel.operation_options') }}
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
+                                                viewBox="0 0 25 15" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-chevron-down link-arrow">
+                                                <polyline points="6 9 12 15 18 9"></polyline>
+                                            </svg>
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item d-flex align-items-center"
+                                                href="{{ route('admin.playlists.edit', $playlist->id) }}">
+                                                <i data-feather="edit-2" class="icon-sm me-2"></i>
+                                                <span class="">{{ __('panel.operation_edit') }}</span>
+                                            </a>
+
+                                            <a href="javascript:void(0);"
+                                                onclick="confirmDelete('delete-playlist-{{ $playlist->id }}', '{{ __('panel.confirm_delete_message') }}', '{{ __('panel.yes_delete') }}', '{{ __('panel.cancel') }}')"
+                                                class="dropdown-item d-flex align-items-center">
+                                                <i data-feather="trash" class="icon-sm me-2"></i>
+                                                <span class="">{{ __('panel.operation_delete') }}</span>
+                                            </a>
+                                            <form action="{{ route('admin.playlists.destroy', $playlist->id) }}"
+                                                method="post" class="d-none" id="delete-playlist-{{ $playlist->id }}">
+                                                @csrf
+                                                @method('DELETE')
+                                            </form>
+
+                                            <a href="javascript:void(0);"
+                                                class="dropdown-item d-flex align-items-center btn btn-success copyButton"
+                                                data-copy-text="https://ibbuniv.era-t.com/playlists/{{ $playlist->slug }}"
+                                                data-id="{{ $playlist->id }}" title="Copy the link">
+                                                <i data-feather="copy" class="icon-sm me-2"></i>
+                                                <span class="">{{ __('panel.operation_copy_link') }}</span>
+                                            </a>
+
+                                        </div>
+                                        <span class="copyMessage" data-id="{{ $playlist->id }}" style="display:none;">
+                                            {{ __('panel.copied') }}
+                                        </span>
+                                    </div>
+                                </div>
                             </td>
                         </tr>
                     @empty
