e667f4db6ec4c7868793bb851763d23f7ee4a0c6
khaleel raweh <khaleelvisa@gmail.com>
2024-12-01 16:06:55 +0300

    save all

--- resources/views/backend/main_sliders/edit.blade.php
+++ resources/views/backend/main_sliders/edit.blade.php
@@ -250,42 +250,47 @@
                     {{-- Published Tab --}}
                     <div class="tab-pane fade" id="published" role="tabpanel" aria-labelledby="published-tab">
 
-                        {{-- published_on   --}}
                         <div class="row">
-                            <div class="col-sm-12 col-md-12 pt-4">
-                                <div class="form-group">
-                                    <label for="published_on"> {{ __('panel.published_date') }}</label>
-                                    <div class="input-group flatpickr" id="flatpickr-datetime">
-                                        <input type="text" name="published_on" class="form-control"
-                                            placeholder="Select date" data-input
-                                            value="{{ old('published_on', Carbon::parse($mainSlider->published_on)->format('Y/m/d h:i A')) }}">
-                                        <span class="input-group-text input-group-addon" data-toggle>
-                                            <i data-feather="calendar"></i>
-                                        </span>
-                                    </div>
-                                    @error('published_on')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
+                            <div class="col-sm-12 col-md-2 pt-3">
+                                {{ __('panel.published_on') }}
+                            </div>
+                            <div class="col-sm-12 col-md-10 pt-3">
+                                <div class="input-group flatpickr" id="flatpickr-datetime">
+                                    <input type="text" name="published_on" class="form-control"
+                                        placeholder="Select date" data-input
+                                        value="{{ old('published_on', $mainSlider->published_on ? \Carbon\Carbon::parse($mainSlider->published_on)->format('Y/m/d h:i A') : '') }}">
+                                    <span class="input-group-text input-group-addon" data-toggle>
+                                        <i data-feather="calendar"></i>
+                                    </span>
                                 </div>
+                                @error('published_on')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
 
 
                         <div class="row">
-                            <div class="col-md-12 col-sm-12 pt-3">
-                                <label for="status" class="control-label col-md-2 col-sm-12 ">
+                            <div class="col-sm-12 col-md-2 pt-3">
+                                <label for="status" class="control-label">
                                     <span>{{ __('panel.status') }}</span>
                                 </label>
-                                <select name="status" class="form-control">
-                                    <option value="1"
-                                        {{ old('status', $mainSlider->status) == '1' ? 'selected' : null }}>
+                            </div>
+                            <div class="col-sm-12 col-md-10 pt-3">
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" class="form-check-input" name="status" id="status_active"
+                                        value="1" {{ old('status', $mainSlider->status) == '1' ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="status_active">
                                         {{ __('panel.status_active') }}
-                                    </option>
-                                    <option value="0"
-                                        {{ old('status', $mainSlider->status) == '0' ? 'selected' : null }}>
+                                    </label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input type="radio" class="form-check-input" name="status" id="status_inactive"
+                                        value="0" {{ old('status', $mainSlider->status) == '0' ? 'checked' : '' }}>
+                                    <label class="form-check-label" for="status_inactive">
                                         {{ __('panel.status_inactive') }}
-                                    </option>
-                                </select>
+                                    </label>
+                                </div>
                                 @error('status')
                                     <span class="text-danger">{{ $message }}</span>
                                 @enderror
