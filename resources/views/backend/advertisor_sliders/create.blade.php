85255005e67c433eea3a03f0ba14a07d99817094
khaleel raweh <khaleelvisa@gmail.com>
2024-12-01 18:36:39 +0300

    save all

--- resources/views/backend/advertisor_sliders/create.blade.php
+++ resources/views/backend/advertisor_sliders/create.blade.php
@@ -478,7 +478,7 @@
                                 {{ __('panel.save_data') }}
                             </button>
 
-                            <a href="{{ route('admin.main_sliders.index') }}" name="submit"
+                            <a href="{{ route('admin.advertisor_sliders.index') }}" name="submit"
                                 class=" btn btn-outline-danger">
                                 <i class="icon-lg  me-2" data-feather="x"></i>
                                 {{ __('panel.cancel') }}
