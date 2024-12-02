0013ee7d3bea1d117d0600d9fe2c33d493018b0b
khaleel raweh <khaleelvisa@gmail.com>
2024-12-02 20:49:02 +0300

    save all

--- resources/views/backend/main_menus/edit.blade.php
+++ resources/views/backend/main_menus/edit.blade.php
@@ -143,61 +143,6 @@
                             </div>
                         </div>
 
-                        {{-- <div class="row">
-                            <div class="col-sm-12 pt-3">
-                                <label for="parent_id" class="control-label">
-                                    {{ __('panel.category_menu') }}
-                                </label>
-                                <select name="parent_id" class="form-control">
-                                    <option value="">{{ __('panel.main_category') }} __</option>
-
-                                    @foreach ($main_menus->where('section', 1) as $main_menu)
-                                        @if (count($main_menu->appearedChildren) == false)
-                                            <option style="color: black;font-weight: bold;font-size:18px;"
-                                                value="{{ $main_menu->id }}"
-                                                {{ old('parent_id', $mainMenu->parent_id) == $main_menu->id ? 'selected' : null }}>
-                                                {{ $main_menu->title }}
-                                            </option>
-                                        @else
-                                            <option style="color: black;font-weight: bold;font-size:18px;"
-                                                value="{{ $main_menu->id }}"
-                                                {{ old('parent_id', $mainMenu->parent_id) == $main_menu->id ? 'selected' : null }}>
-                                                {{ $main_menu->title }}
-                                            </option>
-                                            @if ($main_menu->appearedChildren !== null && count($main_menu->appearedChildren) > 0)
-                                                @foreach ($main_menu->appearedChildren as $sub_menu)
-                                                    @if (count($sub_menu->appearedChildren) == false)
-                                                        <option style="color:blue;font-weight:bold;font-size:15px;"
-                                                            value="{{ $sub_menu->id }}"
-                                                            {{ old('parent_id', $mainMenu->parent_id) == $sub_menu->id ? 'selected' : null }}>
-                                                            &nbsp; &nbsp; &nbsp;{{ $sub_menu->title }}
-                                                        </option>
-                                                    @else
-                                                        <option style="color:blue;font-weight:bold;font-size:15px;"
-                                                            value="{{ $sub_menu->id }}"
-                                                            {{ old('parent_id', $mainMenu->parent_id) == $sub_menu->id ? 'selected' : null }}>
-                                                            &nbsp; &nbsp; &nbsp;{{ $sub_menu->title }}
-                                                        </option>
-                                                        @if ($sub_menu->appearedChildren !== null && count($sub_menu->appearedChildren) > 0)
-                                                            @foreach ($sub_menu->appearedChildren as $sub_menu_2)
-                                                                <option style="font-size: 14px;"
-                                                                    value="{{ $sub_menu_2->id }}"
-                                                                    {{ old('parent_id', $mainMenu->parent_id) == $sub_menu_2->id ? 'selected' : null }}>
-                                                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                                                                    &nbsp;{{ $sub_menu_2->title }}
-                                                                </option>
-                                                            @endforeach
-                                                        @endif
-                                                    @endif
-                                                @endforeach
-                                            @endif
-                                            </li>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div> --}}
-
                         <div class="row ">
                             @foreach (config('locales.languages') as $key => $val)
                                 <div class="col-sm-12 col-md-6 pt-3">
