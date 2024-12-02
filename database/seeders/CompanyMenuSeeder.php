0089f75090c600e78f722ecdd819b240f97111b8
khaleel raweh <khaleelvisa@gmail.com>
2024-12-03 00:50:44 +0300

    save all

--- database/seeders/CompanyMenuSeeder.php
+++ database/seeders/CompanyMenuSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Menu;
-use App\Models\WebMenu;
 use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
