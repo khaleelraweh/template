ed871957d919863861f9733bb9e055942b6a2d34
khaleel raweh <khaleelvisa@gmail.com>
2024-12-02 20:35:51 +0300

    save all

--- resources/views/backend/main_menus/create.blade.php
+++ resources/views/backend/main_menus/create.blade.php
@@ -13,7 +13,7 @@
             <div class="card-naving">
                 <h3 class="font-weight-bold text-primary">
                     <i class="fa fa-plus-square"></i>
-                    {{ __('panel.add_new_link') }}
+                    {{ __('panel.add_new_main_menu_item') }}
                 </h3>
                 <ul class="breadcrumb pt-3">
                     <li>
