625c26f94c75fcbeffaa21344022eaaca9575b2f
khaleel raweh <khaleelvisa@gmail.com>
2024-12-02 17:35:09 +0300

    save all

--- resources/views/backend/tags/filter/filter.blade.php
+++ resources/views/backend/tags/filter/filter.blade.php
@@ -36,9 +36,9 @@
                             {{ old('sort_by', request()->input('sort_by')) == 'id' ? 'selected' : '' }}>
                             {{ __('panel.id') }}
                         </option>
-                        <option value="title"
-                            {{ old('sort_by', request()->input('sort_by')) == 'title' ? 'selected' : '' }}>
-                            {{ __('panel.title') }}
+                        <option value="name"
+                            {{ old('sort_by', request()->input('sort_by')) == 'name' ? 'selected' : '' }}>
+                            {{ __('panel.name') }}
                         </option>
                     </select>
                 </div>
