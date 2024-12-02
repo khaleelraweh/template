5d6f5d06c0c4c766da52e9926f250c695ae81322
khaleel raweh <khaleelvisa@gmail.com>
2024-12-02 17:48:00 +0300

    save all

--- resources/views/backend/tags/create.blade.php
+++ resources/views/backend/tags/create.blade.php
@@ -66,18 +66,23 @@
 
                         @foreach (config('locales.languages') as $key => $val)
                             <div class="row ">
-                                <div class="col-sm-12 pt-3">
-                                    <div class="form-group">
-                                        <label for="name[{{ $key }}]">{{ __('panel.tag_name') }}
-                                            ({{ $key }})
-                                        </label>
-                                        <input type="text" name="name[{{ $key }}]"
-                                            id="name[{{ $key }}]" value="{{ old('name.' . $key) }}"
-                                            class="form-control">
-                                        @error('name.' . $key)
-                                            <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
+                                <div class="col-sm-12 col-md-2 pt-3">
+                                    <label for="title[{{ $key }}]">
+                                        {{ __('panel.tag_name') }}
+                                        <span class="language-type">
+                                            <i class="flag-icon flag-icon-{{ $key == 'ar' ? 'ye' : 'us' }} mt-1 "
+                                                title="{{ app()->getLocale() == 'ar' ? 'ye' : 'us' }}"></i>
+                                            {{ __('panel.' . $key) }}
+                                        </span>
+                                    </label>
+                                </div>
+
+                                <div class="col-sm-12 col-md-10 pt-3">
+                                    <input type="text" name="name[{{ $key }}]" id="name[{{ $key }}]"
+                                        value="{{ old('name.' . $key) }}" class="form-control">
+                                    @error('name.' . $key)
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                         @endforeach
