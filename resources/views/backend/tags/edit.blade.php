cf95830d438f4a592c323a6883e86019c916dabd
khaleel raweh <khaleelvisa@gmail.com>
2024-12-02 18:04:43 +0300

    save all

--- resources/views/backend/tags/edit.blade.php
+++ resources/views/backend/tags/edit.blade.php
@@ -12,16 +12,16 @@
                     <i class="fa fa-edit"></i>
                     {{ __('panel.edit_existing_tag') }}
                 </h3>
-                <ul class="breadcrumb">
+                <ul class="breadcrumb pt-3">
                     <li>
                         <a href="{{ route('admin.index') }}">{{ __('panel.main') }}</a>
                         @if (config('locales.languages')[app()->getLocale()]['rtl_support'] == 'rtl')
-                            <i class="fa fa-solid fa-chevron-left chevron"></i>
+                            \
                         @else
-                            <i class="fa fa-solid fa-chevron-right chevron"></i>
+                            /
                         @endif
                     </li>
-                    <li>
+                    <li class="ms-1">
                         <a href="{{ route('admin.tags.index') }}">
                             {{ __('panel.show_tags') }}
                         </a>
@@ -48,130 +48,52 @@
                 @csrf
                 @method('PATCH')
 
-                {{-- links of tabs --}}
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="content-tab" data-bs-toggle="tab" data-bs-target="#content"
-                            type="button" role="tab" aria-controls="content"
-                            aria-selected="true">{{ __('panel.content_tab') }}</button>
-                    </li>
-
-
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="published-tab" data-bs-toggle="tab" data-bs-target="#published"
-                            type="button" role="tab" aria-controls="published"
-                            aria-selected="false">{{ __('panel.published_tab') }}</button>
-                    </li>
-
-                </ul>
-
-                <div class="tab-content" id="myTabContent">
-
-                    <div class="tab-pane fade active show" id="content" role="tabpanel" aria-labelledby="content-tab">
-
-                        @foreach (config('locales.languages') as $key => $val)
-                            <div class="row ">
-                                <div class="col-sm-12 pt-3">
-                                    <div class="form-group">
-                                        <label for="name[{{ $key }}]">{{ __('panel.tag_name') }}
-                                            ({{ $key }})
-                                        </label>
-                                        <input type="text" name="name[{{ $key }}]"
-                                            id="name[{{ $key }}]"
-                                            value="{{ old('name.' . $key, $tag->getTranslation('name', $key)) }}"
-                                            class="form-control">
-                                        @error('name.' . $key)
-                                            <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-
-                        <div class="row">
-                            <div class="col-sm-12 pt-4">
-                                <label for="section">{{ __('panel.tag_type') }}</label>
-                                <select name="section" class="form-control">
-                                    <option value="1" {{ old('section', $tag->section) == '1' ? 'selected' : null }}>
-                                        {{ __('panel.course_tag') }}
-                                    </option>
-                                    <option value="2" {{ old('section', $tag->section) == '2' ? 'selected' : null }}>
-                                        {{ __('panel.event_tag') }}
-                                    </option>
-                                    <option value="3" {{ old('section', $tag->section) == '3' ? 'selected' : null }}>
-                                        {{ __('panel.post_tag') }}
-                                    </option>
-                                </select>
-                                @error('section')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                        </div>
-                    </div>
-
-                    {{-- Publish Tab --}}
-                    <div class="tab-pane fade" id="published" role="tabpanel" aria-labelledby="published-tab">
-
-                        <div class="row">
-                            <div class="col-sm-12 col-md-12 pt-4">
-                                <div class="form-group">
-                                    <label for="published_on">{{ __('panel.published_date') }} </label>
-                                    <input type="text" id="published_on" name="published_on"
-                                        value="{{ old('published_on', \Carbon\Carbon::parse($tag->published_on)->Format('Y-m-d')) }}"
-                                        class="form-control">
-                                    @error('published_on')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div class="row">
-                            <div class="col-sm-12 col-md-12 pt-4">
-                                <div class="form-group">
-                                    <label for="published_on_time"> {{ __('panel.published_time') }}</label>
-                                    <input type="text" id="published_on_time" name="published_on_time"
-                                        value="{{ old('published_on_time', \Carbon\Carbon::parse($tag->published_on)->Format('h:i A')) }}"
-                                        class="form-control">
-                                    @error('published_on_time')
-                                        <span class="text-danger">{{ $message }}</span>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-12 col-sm-12 pt-3">
-                                <label for="status" class="control-label col-md-2 col-sm-12 ">
-                                    <span>{{ __('panel.status') }}</span>
+                @foreach (config('locales.languages') as $key => $val)
+                    <div class="row ">
+                        <div class="col-sm-12 pt-3">
+                            <div class="form-group">
+                                <label for="name[{{ $key }}]">{{ __('panel.tag_name') }}
+                                    ({{ $key }})
                                 </label>
-                                <select name="status" class="form-control">
-                                    <option value="1" {{ old('status', $tag->status) == '1' ? 'selected' : null }}>
-                                        {{ __('panel.status_active') }}
-                                    </option>
-                                    <option value="0" {{ old('status', $tag->status) == '0' ? 'selected' : null }}>
-                                        {{ __('panel.status_inactive') }}
-                                    </option>
-                                </select>
-                                @error('status')
+                                <input type="text" name="name[{{ $key }}]" id="name[{{ $key }}]"
+                                    value="{{ old('name.' . $key, $tag->getTranslation('name', $key)) }}"
+                                    class="form-control">
+                                @error('name.' . $key)
                                     <span class="text-danger">{{ $message }}</span>
                                 @enderror
                             </div>
                         </div>
-
                     </div>
+                @endforeach
 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="form-group pt-3">
-                                <button type="submit" name="submit" class="btn btn-primary">
-                                    {{ __('panel.update_data') }}
-                                </button>
-                            </div>
+                <div class="row">
+                    <div class="col-sm-12 pt-4">
+                        <label for="section">{{ __('panel.tag_type') }}</label>
+                        <select name="section" class="form-control">
+                            <option value="1" {{ old('section', $tag->section) == '1' ? 'selected' : null }}>
+                                {{ __('panel.course_tag') }}
+                            </option>
+                            <option value="2" {{ old('section', $tag->section) == '2' ? 'selected' : null }}>
+                                {{ __('panel.event_tag') }}
+                            </option>
+                            <option value="3" {{ old('section', $tag->section) == '3' ? 'selected' : null }}>
+                                {{ __('panel.post_tag') }}
+                            </option>
+                        </select>
+                        @error('section')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group pt-3">
+                            <button type="submit" name="submit" class="btn btn-primary">
+                                {{ __('panel.update_data') }}
+                            </button>
                         </div>
                     </div>
-
                 </div>
 
             </form>
