017efaf0987c26419fe9032a3bcef9dd7ab870d9
khaleel raweh <khaleelvisa@gmail.com>
2024-12-03 21:16:49 +0300

    save all

--- app/Http/Controllers/Backend/PresidentSpeechController.php
+++ app/Http/Controllers/Backend/PresidentSpeechController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Backend\PresidentSpeechRequest;
 use App\Models\PresidentSpeech;
+use Carbon\Carbon;
 use DateTimeImmutable;
 use Illuminate\Http\Request;
 use Intervention\Image\Drivers\Gd\Driver;
@@ -29,10 +30,10 @@ class PresidentSpeechController extends Controller
             ->when(\request()->status != null, function ($query) {
                 $query->where('status', \request()->status);
             })
-            ->orderBy(\request()->sort_by ?? 'created_at', \request()->order_by ?? 'desc')
-            ->paginate(\request()->limit_by ?? 10);
-
-
+            ->orderByRaw(request()->sort_by == 'published_on'
+                ? 'published_on IS NULL, published_on ' . (request()->order_by ?? 'desc')
+                : (request()->sort_by ?? 'created_at') . ' ' . (request()->order_by ?? 'desc'))
+            ->paginate(\request()->limit_by ?? 100);
 
         return view('backend.president_speeches.index', compact('president_speeches'));
     }
@@ -53,12 +54,33 @@ class PresidentSpeechController extends Controller
         }
 
 
-
         $input['title'] = $request->title;
         $input['content'] = $request->content;
 
+        $input['metadata_title'] = [];
+        foreach (config('locales.languages') as $localeKey => $localeValue) {
+            $input['metadata_title'][$localeKey] = $request->metadata_title[$localeKey]
+                ?: $request->title[$localeKey] ?? null;
+        }
+        $input['metadata_description'] = [];
+        foreach (config('locales.languages') as $localeKey => $localeValue) {
+            $content = $request->content[$localeKey] ?? '';
+            // Remove all tags and decode HTML entities
+            $plainContent = html_entity_decode(strip_tags($content), ENT_QUOTES | ENT_HTML5);
+            // Limit to 30 words
+            $limitedContent = implode(' ', array_slice(explode(' ', $plainContent), 0, 30));
+            $input['metadata_description'][$localeKey] = $request->metadata_description[$localeKey]
+                ?: $limitedContent ?: null;
+        }
+        $input['metadata_keywords']     = $request->metadata_keywords;
+
+
         $input['status']            =   $request->status;
         $input['created_by'] = auth()->user()->full_name;
+
+        $published_on = str_replace(['ص', 'م'], ['AM', 'PM'], $request->published_on);
+        $publishedOn = Carbon::createFromFormat('Y/m/d h:i A', $published_on)->format('Y-m-d H:i:s');
+        $input['published_on']            = $publishedOn;
 
 
         if ($image = $request->file('promotional_image')) {
@@ -73,10 +95,10 @@ class PresidentSpeechController extends Controller
             $input['promotional_image'] = $file_name;
         }
 
-        $about_instatute = PresidentSpeech::create($input);
+        $president_speech = PresidentSpeech::create($input);
 
 
-        if ($about_instatute) {
+        if ($president_speech) {
             return redirect()->route('admin.president_speeches.index')->with([
                 'message' => __('panel.created_successfully'),
                 'alert-type' => 'success'
@@ -99,16 +121,16 @@ class PresidentSpeechController extends Controller
         return view('backend.president_speeches.show');
     }
 
-    public function edit($about_instatute)
+    public function edit($president_speech)
     {
         if (!auth()->user()->ability('admin', 'update_president_speeches')) {
             return redirect('admin/index');
         }
 
 
-        $about_instatute = PresidentSpeech::where('id', $about_instatute)->first();
+        $president_speech = PresidentSpeech::where('id', $president_speech)->first();
 
-        return view('backend.president_speeches.edit', compact('about_instatute'));
+        return view('backend.president_speeches.edit', compact('president_speech'));
     }
 
     public function update(PresidentSpeechRequest $request, $about_instatute)
