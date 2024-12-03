88edb2d607ff8a9db3fe7955bc785410a3effa4b
khaleel raweh <khaleelvisa@gmail.com>
2024-12-03 22:39:34 +0300

    save all

--- resources/views/backend/common_questions/index.blade.php
+++ resources/views/backend/common_questions/index.blade.php
@@ -56,31 +56,80 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse ($common_questions as $question)
+                        @forelse ($common_questions as $common_question)
                             <tr>
                                 <td>
-                                    {{ Str::limit($question->title, 50) }}
+                                    {{ Str::limit($common_question->title, 50) }}
                                 </td>
-                                <td>{{ $question->created_by }}</td>
-                                <td>{{ $question->status() }}</td>
-                                <td>{{ $question->created_at->format('Y-m-d h:i a') }}</td>
+                                <td>{{ $common_question->created_by }}</td>
+                                <td>{{ $common_question->status() }}</td>
+                                <td>{{ $common_question->created_at->format('Y-m-d h:i a') }}</td>
                                 <td>
-                                    <div class="btn-group btn-group-sm">
-                                        <a href="{{ route('admin.common_questions.edit', $question->id) }}"
+                                    {{-- <div class="btn-group btn-group-sm">
+                                        <a href="{{ route('admin.common_questions.edit', $common_question->id) }}"
                                             class="btn btn-primary">
                                             <i class="fa fa-edit"></i>
                                         </a>
                                         <a href="javascript:void(0);"
-                                            onclick=" if( confirm('{{ __('panel.confirm_delete_message') }}') ){document.getElementById('delete-question-{{ $question->id }}').submit();}else{return false;}"
+                                            onclick=" if( confirm('{{ __('panel.confirm_delete_message') }}') ){document.getElementById('delete-question-{{ $common_question->id }}').submit();}else{return false;}"
                                             class="btn btn-danger">
                                             <i class="fa fa-trash"></i>
                                         </a>
                                     </div>
-                                    <form action="{{ route('admin.common_questions.destroy', $question->id) }}"
-                                        method="post" class="d-none" id="delete-question-{{ $question->id }}">
+                                    <form action="{{ route('admin.common_questions.destroy', $common_question->id) }}"
+                                        method="post" class="d-none" id="delete-question-{{ $common_question->id }}">
                                         @csrf
                                         @method('DELETE')
-                                    </form>
+                                    </form> --}}
+                                    <div class="btn-group btn-group-sm">
+                                        <div class="dropdown mb-2 ">
+                                            <a type="button" class="d-flex" id="dropdownMenuButton"
+                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="icon-lg text-muted pb-3px" data-feather="more-vertical"></i>
+                                                {{ __('panel.operation_options') }}
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
+                                                    viewBox="0 0 25 15" fill="none" stroke="currentColor"
+                                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-chevron-down link-arrow">
+                                                    <polyline points="6 9 12 15 18 9"></polyline>
+                                                </svg>
+                                            </a>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item d-flex align-items-center"
+                                                    href="{{ route('admin.common_questions.edit', $common_question->id) }}">
+                                                    <i data-feather="edit-2" class="icon-sm me-2"></i>
+                                                    <span class="">{{ __('panel.operation_edit') }}</span>
+                                                </a>
+
+                                                <a href="javascript:void(0);"
+                                                    onclick="confirmDelete('delete-common_question-{{ $common_question->id }}', '{{ __('panel.confirm_delete_message') }}', '{{ __('panel.yes_delete') }}', '{{ __('panel.cancel') }}')"
+                                                    class="dropdown-item d-flex align-items-center">
+                                                    <i data-feather="trash" class="icon-sm me-2"></i>
+                                                    <span class="">{{ __('panel.operation_delete') }}</span>
+                                                </a>
+                                                <form
+                                                    action="{{ route('admin.common_questions.destroy', $common_question->id) }}"
+                                                    method="post" class="d-none"
+                                                    id="delete-common_question-{{ $common_question->id }}">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                </form>
+
+                                                <a href="javascript:void(0);"
+                                                    class="dropdown-item d-flex align-items-center btn btn-success copyButton"
+                                                    data-copy-text="https://ibbuniv.era-t.com/common_questions/{{ $common_question->slug }}"
+                                                    data-id="{{ $common_question->id }}" title="Copy the link">
+                                                    <i data-feather="copy" class="icon-sm me-2"></i>
+                                                    <span class="">{{ __('panel.operation_copy_link') }}</span>
+                                                </a>
+
+                                            </div>
+                                            <span class="copyMessage" data-id="{{ $common_question->id }}"
+                                                style="display:none;">
+                                                {{ __('panel.copied') }}
+                                            </span>
+                                        </div>
+                                    </div>
                                 </td>
                             </tr>
                         @empty
