12a6ea66e2ddd973c510edbecfacbcd6afda8edb
khaleel raweh <khaleelvisa@gmail.com>
2024-12-31 01:15:56 +0300

    save all

--- app/Http/Livewire/Frontend/Blogs/BlogListComponent.php
+++ app/Http/Livewire/Frontend/Blogs/BlogListComponent.php
@@ -15,7 +15,7 @@ class BlogListComponent extends Component
     use LivewireAlert;
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
-    public $paginationLimit = 1;
+    public $paginationLimit = 4;
 
     public $slug;
 
