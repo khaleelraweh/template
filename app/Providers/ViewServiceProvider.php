524b621241eb75680e2496a84c01b5fbf3e5bdff
khaleel raweh <khaleelvisa@gmail.com>
2024-12-31 22:21:18 +0300

    save all

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -26,27 +26,24 @@ class ViewServiceProvider extends ServiceProvider
     public function boot()
     {
         //check if request is admin 
-        if(request()->is('admin/*')){ 
+        if (request()->is(env('APP_URL') . 'admin/*')) {
 
             //send cache to every view under admin 
             View()->composer('*', function ($view) {
 
-                if(!Cache::has('admin_side_menu')){
+                if (!Cache::has('admin_side_menu')) {
                     //make cache with  main permition come from permission model tree function
                     Cache::forever('admin_side_menu', Permission::tree());
                 }
-    
+
                 $admin_side_menu = Cache::get('admin_side_menu');
                 $view->with(
                     [
                         //send admin side menu cache as varaible to view page
-                        'admin_side_menu'=>$admin_side_menu
+                        'admin_side_menu' => $admin_side_menu
                     ]
                 );
             });
-
-            
-
         }
     }
 }
