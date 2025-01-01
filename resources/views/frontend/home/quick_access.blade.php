a32c136221669c68006e0fc12220bae6db88c933
khaleel raweh <khaleelvisa@gmail.com>
2025-01-01 16:30:37 +0300

    save all

--- resources/views/frontend/home/quick_access.blade.php
+++ resources/views/frontend/home/quick_access.blade.php
@@ -4,37 +4,55 @@
             <h2 class="title-1 mb-0 qa-title-line text-white">{{ __('panel.quick_access') }}</h2>
         </div>
         <div class="row">
-            <div class="col-sm-6 col-md-4 col-lg-2">
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
 
-                <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                    alt="Icon">
-                <h5 class="text-white text-center">محاضرات الأقسام</h5>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-2">
-                <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                    alt="Icon">
-                <h5 class="text-white text-center">محاضرات الأقسام</h5>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-2">
-                <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                    alt="Icon">
-                <h5 class="text-white text-center">محاضرات الأقسام</h5>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-2">
-                <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                    alt="Icon">
-                <h5 class="text-white text-center">محاضرات الأقسام</h5>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-2">
-                <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                    alt="Icon">
-                <h5 class="text-white text-center">محاضرات الأقسام</h5>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 ">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-2">
-                <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                    alt="Icon">
-                <h5 class="text-white text-center">محاضرات الأقسام</h5>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2  ">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+
             </div>
+
+
         </div>
     </div>
 </div>
