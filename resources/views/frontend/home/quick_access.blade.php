5da138a230e5687d64a0bfbab3f25baee9a70c77
khaleel raweh <khaleelvisa@gmail.com>
2025-01-01 15:14:11 +0300

    save all

---
+++ resources/views/frontend/home/quick_access.blade.php
@@ -0,0 +1,57 @@
+<div class="image-main">
+    <div class="container mt-5 serv">
+        <div class="sec-title mb-60 md-mb-30 text-left">
+            <h2 class="title-1 mb-0 header_playlist">الوصول السريع</h2>
+        </div>
+        <div class="mbbtt">
+            <div class="row mbbt row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
+                <div class="col pp">
+                    <div class="card-1">
+                        <img src="{{ asset('frontend/images/waleed/Vector.png') }}" class="card-img-top" alt="Icon">
+                        <div class="card-body">
+                            <h5 class="card-title-1">محاضرات الأقسام</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col pp">
+                    <div class="card-1">
+                        <img src="{{ asset('frontend/images/waleed/Vector.png') }}" class="card-img-top" alt="Icon">
+                        <div class="card-body ppcar">
+                            <h5 class="card-title-1">محاضرات الأقسام</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col pp">
+                    <div class="card-1">
+                        <img src="{{ asset('frontend/images/waleed/Vector.png') }}" class="card-img-top" alt="Icon">
+                        <div class="card-body">
+                            <h5 class="card-title-1">محاضرات الأقسام</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col pp">
+                    <div class="card-1">
+                        <img src="{{ asset('frontend/images/waleed/Vector.png') }}" class="card-img-top" alt="Icon">
+                        <div class="card-body">
+                            <h5 class="card-title-1">محاضرات الأقسام</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col pp">
+                    <div class="card-1">
+                        <img src="{{ asset('frontend/images/waleed/Vector.png') }}" class="card-img-top" alt="Icon">
+                        <div class="card-body">
+                            <h5 class="card-title-1">محاضرات الأقسام</h5>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
