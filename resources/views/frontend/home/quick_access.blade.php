caf618d53cb2e41ed2a8781afef4bca581d8a4eb
mohalyemeni <mohalyemeni2009@gmail.com>
2025-01-13 16:31:34 +0300

    save all

--- resources/views/frontend/home/quick_access.blade.php
+++ resources/views/frontend/home/quick_access.blade.php
@@ -4,76 +4,76 @@
             <h2 class="title-1 mb-0 qa-title-line text-white">{{ __('panel.quick_access') }}</h2>
         </div>
         <div class="row">
-            @foreach ($main_sliders->where('section', 2)->take(10) as $adv_slider)
-            <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4 d-flex justify-content-stretch align-items-stretch">
-                <div class="qa-content p-3 flex-fill">
-                    @php
-                    if ($adv_slider->firstMedia != null && $adv_slider->firstMedia->file_name != null) {
-                    $advertisor_slider_img = asset(
-                    'assets/advertisor_sliders/' . $adv_slider->firstMedia->file_name,
-                    );
+            @foreach ($main_sliders->where('section', 2)->take(3) as $adv_slider)
+                <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4 d-flex justify-content-stretch align-items-stretch">
+                    <div class="qa-content p-3 flex-fill">
+                        @php
+                            if ($adv_slider->firstMedia != null && $adv_slider->firstMedia->file_name != null) {
+                                $advertisor_slider_img = asset(
+                                    'assets/advertisor_sliders/' . $adv_slider->firstMedia->file_name,
+                                );
 
-                    if (
-                    !file_exists(
-                    public_path('assets/advertisor_sliders/' . $adv_slider->firstMedia->file_name),
-                    )
-                    ) {
-                    $advertisor_slider_img = asset('frontend/images/quick_access/icon/1.png');
-                    }
-                    } else {
-                    $advertisor_slider_img = asset('frontend/images/quick_access/icon/1.png');
-                    }
-                    @endphp
-                    <img style="width: 5.06em;height:5.43em" src="{{ $advertisor_slider_img }}"
-                        class="d-block my-0 mb-4 mx-auto" alt="Icon">
-                    <h5 class="text-white text-center">{{ $adv_slider->title }}</h5>
+                                if (
+                                    !file_exists(
+                                        public_path('assets/advertisor_sliders/' . $adv_slider->firstMedia->file_name),
+                                    )
+                                ) {
+                                    $advertisor_slider_img = asset('frontend/images/quick_access/icon/1.png');
+                                }
+                            } else {
+                                $advertisor_slider_img = asset('frontend/images/quick_access/icon/1.png');
+                            }
+                        @endphp
+                        <img style="width: 5.06em;height:5.43em" src="{{ $advertisor_slider_img }}"
+                            class="d-block my-0 mb-4 mx-auto" alt="Icon">
+                        <h5 class="text-white text-center">{{ $adv_slider->title }}</h5>
+                    </div>
+
                 </div>
-
-            </div>
             @endforeach
 
 
             {{-- <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
                 <div class="qa-content p-3">
                     <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-            alt="Icon">
-            <h5 class="text-white text-center">محاضرات الأقسام</h5>
-        </div>
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
 
+            </div>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+
+            </div>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+            </div>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2 ">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+
+            </div>
+            <div class="col-6  col-md-4 col-lg-4 col-xl-2  ">
+                <div class="qa-content p-3">
+                    <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
+                        alt="Icon">
+                    <h5 class="text-white text-center">محاضرات الأقسام</h5>
+                </div>
+
+            </div> --}}
+
+
+        </div>
     </div>
-    <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
-        <div class="qa-content p-3">
-            <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                alt="Icon">
-            <h5 class="text-white text-center">محاضرات الأقسام</h5>
-        </div>
-
-    </div>
-    <div class="col-6  col-md-4 col-lg-4 col-xl-2 mb-4">
-        <div class="qa-content p-3">
-            <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                alt="Icon">
-            <h5 class="text-white text-center">محاضرات الأقسام</h5>
-        </div>
-    </div>
-    <div class="col-6  col-md-4 col-lg-4 col-xl-2 ">
-        <div class="qa-content p-3">
-            <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                alt="Icon">
-            <h5 class="text-white text-center">محاضرات الأقسام</h5>
-        </div>
-
-    </div>
-    <div class="col-6  col-md-4 col-lg-4 col-xl-2  ">
-        <div class="qa-content p-3">
-            <img src="{{ asset('frontend/images/quick_access/icon/1.png') }}" class="d-block my-0 mb-4 mx-auto"
-                alt="Icon">
-            <h5 class="text-white text-center">محاضرات الأقسام</h5>
-        </div>
-
-    </div> --}}
-
-
-</div>
-</div>
 </div>
